3862e91354bcf69fefd2ccb067672a2c00e9b7ed
Stephen <stephen.s.espinosa@gmail.com>
2023-06-29 23:16:51 +0200

    change little in js

--- index.js
+++ index.js
@@ -8,51 +8,56 @@ for (var i = 0; i < drumKey.length; i++) {
 
 function mouseClick() {
   var buttonInnerHTML = this.innerHTML;
-  console.log("click");
   makeSound(buttonInnerHTML);
 }
 
 document.addEventListener("keydown", function (event) {
-  makeSound(event);
+  makeSound(event.key);
 });
 
 function makeSound(key) {
-  console.log(key.target);
-  switch (key.key) {
+  console.log(key);
+  switch (key) {
     case "w":
       var audio = new Audio("/sounds/crash.mp3");
       audio.play();
-      console.log(this.textContent);
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "a":
       var audio = new Audio("/sounds/kick-bass.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "s":
       var audio = new Audio("/sounds/snare.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "d":
       var audio = new Audio("/sounds/tom-1.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "j":
       var audio = new Audio("/sounds/tom-2.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "k":
       var audio = new Audio("/sounds/tom-3.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     case "l":
       var audio = new Audio("/sounds/tom-4.mp3");
       audio.play();
+      document.querySelector(`.${key}`).style.color = "red";
       break;
 
     default:
